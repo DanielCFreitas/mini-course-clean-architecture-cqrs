64c1abf0d670d603f12e9dcce6f9b178cc342da8
Daniel Corrêa <daniel.exatas@gmail.com>
2024-11-27 08:19:44 -0300

    Adicionando manipulador de erros para quando houver exception do dominio da aplicacao

---
+++ src/PackIT/Shared/PackIt.Shared/Exceptions/ExceptionMiddleware.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.Json;
+using Microsoft.AspNetCore.Http;
+using PackIt.Shared.Abstractions.Exceptions;
+
+namespace PackIt.Shared.Exceptions;
+
+internal sealed class ExceptionMiddleware : IMiddleware
+{
+    public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+    {
+        try
+        {
+            await next(context);
+        }
+        catch (PackItException e)
+        {
+            context.Response.StatusCode = 400;
+            context.Response.ContentType = "application/json";
+
+            var errorName = e.GetType().Name.Replace("Exception", string.Empty);
+            var json = JsonSerializer.Serialize(new { ErrorName = errorName, e.Message });
+            await context.Response.WriteAsync(json);
+        }
+    }
+}
--- src/PackIT/Shared/PackIt.Shared/Extensions.cs
+++ src/PackIT/Shared/PackIt.Shared/Extensions.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using PackIt.Shared.Exceptions;
 using PackIt.Shared.Services;
 
 namespace PackIt.Shared;
@@ -8,6 +10,13 @@ public static class Extensions
     public static IServiceCollection AddShared(this IServiceCollection services)
     {
         services.AddHostedService<AppInitializer>();
+        services.AddScoped<ExceptionMiddleware>();
         return services;
+    }
+
+    public static IApplicationBuilder UseShared(this IApplicationBuilder app)
+    {
+        app.UseMiddleware<ExceptionMiddleware>();
+        return app;
     }
 }
